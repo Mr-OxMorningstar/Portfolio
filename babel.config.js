2e7b4dea3505f281d2837a5dc302e2f64d6a3510
agreement <agreement@aions.co.za>
2024-11-08 15:42:23 +0200

    push

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,10 @@
 module.exports = {
     presets: ["@babel/preset-env", "@babel/preset-react"],
-    plugins: [
-      ["@babel/plugin-proposal-class-properties", { loose: true }],
-      ["@babel/plugin-proposal-private-methods", { loose: true }],
-      ["@babel/plugin-proposal-private-property-in-object", { loose: true }],
-    ],
-  };
+        "plugins": [
+          ["@babel/plugin-transform-class-properties", { "loose": true }],
+          ["@babel/plugin-transform-private-methods", { "loose": true }],
+          ["@babel/plugin-proposal-private-property-in-object", { "loose": true }]
+        ]
+      }
+      
   
--- config-overrides.js
+++ config-overrides.js
@@ -1,11 +1,3 @@
-const { override, addBabelPreset, addBabelPlugins } = require("customize-cra");
-
-module.exports = override(
-  addBabelPreset("@babel/preset-env"),
-  addBabelPreset("@babel/preset-react"),
-  addBabelPlugins(
-    ["@babel/plugin-proposal-class-properties", { loose: true }],
-    ["@babel/plugin-proposal-private-methods", { loose: true }],
-    ["@babel/plugin-proposal-private-property-in-object", { loose: true }]
-  )
-);
+module.exports = function override(config, env) {
+  return config;
+};
